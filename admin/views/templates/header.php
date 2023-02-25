a1d1d807abcc6b7fea430a595b273b5f57803228
BiLaL-Obd <obeidb04@gmail.com>
2023-02-25 17:39:13 +0200

    1

---
+++ admin/views/templates/header.php
@@ -0,0 +1,38 @@
+<body>
+    <div class="page d-flex">
+        <div class="sidebar bg-white p-2 p-md-4 position-relative">
+            <h3 class="position-relative d-none d-md-block font-size-20 txt-c mt-0">LegendStore</h3>
+            <h3 class="d-block d-md-none text-center">LS</h3>
+            <ul>
+                <li>
+                    <a class="active d-flex align-center font-size-14 c-black rad-6 p-10" href="index.html">
+                        <i class='bx bx-line-chart font-size-16'></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="d-flex align-center font-size-14 c-black rad-6 p-10" href="projects.html">
+                        <i class='bx bx-category font-size-16'></i>
+                        <span>Categories</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="d-flex align-center font-size-14 c-black rad-6 p-10" href="courses.html">
+                        <i class='bx bx-store font-size-16'></i>
+                        <span>Products</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="d-flex align-center font-size-14 c-black rad-6 p-10" href="clients.html">
+                        <i class='bx bx-user-circle font-size-16'></i>
+                        <span>Clients</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="d-flex align-center font-size-14 c-black rad-6 p-10" href="#">
+                        <i class='bx bxl-blogger font-size-16'></i>
+                        <span>Blogs</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
