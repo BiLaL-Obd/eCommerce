f4e8e04865614f4f19b724c4937fbbc69cffa74f
BiLaL-Obd <obeidb04@gmail.com>
2023-04-29 17:51:43 +0300

    update info-account

--- info-account.php
+++ info-account.php
@@ -9,27 +9,53 @@
                 <div class="row">
                     <div class="col-12 bg-white rounded-3">
                         <div class="row">
-                            <div class="col-12 col-md-6 mb-3 p-3">
-                                <div class="d-flex align-items-start gap-3">
-                                    <img src="admin/images/about-us.png" class="mb-3 pers-info rounded-circle" alt="">
-                                    <div class="info">
-                                        <div class="mb-3 fw-500 font-size-20">
-                                            Bilal Obeid
-                                        </div>
-                                        <div class="mb-3 font-size-13">
-                                            <span>Date of join :</span>
-                                            1st Jan 1999
+                            <div class="col-12 col-md-6 p-3 info-1">
+                                <div class="d-flex justify-content-between">
+                                    <div class="d-flex align-items-start gap-3">
+                                        <img src="admin/images/about-us.png" class="mb-3 pers-info rounded-circle" alt="">
+                                        <div class="info">
+                                            <div class="mb-3 fw-500 font-size-20">
+                                                Bilal Obeid
+                                            </div>
+                                            <div class="mb-3 font-size-13">
+                                                <span>Date of join :</span>
+                                                1st Jan 1999
+                                            </div>
                                         </div>
                                     </div>
+                                    <button type="button" class="btn btn-danger d-md-none d-block edit-info btn-sm d-flex align-items-center justify-content-center rounded-circle gap-2" data-bs-toggle="modal" data-bs-target="#modalId">
+                                        <i class='bx bx-pencil'></i>
+                                    </button>
                                 </div>
                             </div>
-                            <div class="col-12 col-md-6 mb-3 p-3">
-
-                                <div class="mb-3 font-size-14">
-                                    bilal@gmail.com
-                                </div>
-                                <div class="mb-3 font-size-12">
-                                    11-22-1999
+                            <div class="col-12 info-2 col-md-6 p-3">
+                                <div class="d-flex justify-content-between">
+                                    <div class="d-flex align-items-start gap-3 w-100">
+                                        <div class="pers-info d-none d-sm-block d-md-none" hidden></div>
+                                        <div class="secondary-info w-100">
+                                            <div class="mb-3 font-size-14">
+                                                <span>Phone :</span>
+                                                +96171865646
+                                            </div>
+                                            <div class="mb-3 font-size-14">
+                                                <span>Email :</span>
+                                                <a href="mailto:bilal@gmail.com">
+                                                    bilal@gmail.com
+                                                </a>
+                                            </div>
+                                            <div class="mb-3 font-size-14">
+                                                <span>Address :</span>
+                                                Bakhoun - Halkit
+                                            </div>
+                                            <div class="mb-3 font-size-14">
+                                                <span>Gender :</span>
+                                                Male
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button type="button" class="btn btn-danger d-none d-md-block edit-info btn-sm d-flex align-items-center justify-content-center rounded-circle gap-2" data-bs-toggle="modal" data-bs-target="#modalId">
+                                        <i class='bx bx-pencil'></i>
+                                    </button>
                                 </div>
                             </div>
                             <div class="col-12 px-0">
@@ -46,20 +72,40 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-12">
-                        <div class="tab-content p-1 p-md-3" id="nav-tabContent">
+                    <div class="col-12 px-0 mt-3">
+                        <div class="tab-content" id="nav-tabContent">
                             <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                                <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
-                                    <span class="fw-500 sub-title">Full Name :</span> <span>Bilal Obeid</span>
-                                </div>
-                                <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
-                                    <span class="fw-500 sub-title">Username :</span> <span>@bilal</span>
-                                </div>
-                                <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
-                                    <span class="fw-500 sub-title">Email Addr. :</span> <span>bilal@gmail.com</span>
-                                </div>
-                                <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
-                                    <span class="fw-500 sub-title">Last Updated :</span> <span>19:50pm 11-22-1999</span>
+                                <div class="d-flex align-items-center gap-1">
+                                    <div class="col-12 col-md-6 bg-white rounded-3 p-3">
+                                        <h5 class="mb-4">Personal Inforamtion :</h5>
+                                        <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Phone :</span> <span>+96178789789</span>
+                                        </div>
+                                        <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Notionality :</span> <span>Lebanon</span>
+                                        </div>
+                                        <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Another Addr. :</span> <span>Bakhoun - sir</span>
+                                        </div>
+                                        <div class="d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Purchase count :</span> <span>02</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-12 col-md-6 bg-white rounded-3 p-3">
+                                        <h5 class="mb-4">Emergency Contact :</h5>
+                                        <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Name :</span> <span>Sami</span>
+                                        </div>
+                                        <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Phone :</span> <span>+961789789</span>
+                                        </div>
+                                        <div class="mb-4 d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Addr. :</span> <span>Bakhoun - sir</span>
+                                        </div>
+                                        <div class="d-flex align-items-center gap-2 gap-md-5">
+                                            <span class="fw-500 sub-title">Relationship :</span> <span>Brother</span>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                             <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
@@ -159,42 +205,8 @@
 
 </html>
 <!-- Button trigger modal -->
-<!-- <button type="button" class="btn btn-danger edit-info btn-sm d-flex align-items-center gap-2" data-bs-toggle="modal" data-bs-target="#modalId">
-                            <i class='bx bx-edit'></i>
-                            Edit Details
-                        </button> -->
-<!-- Modal -->
-<!-- <div class="modal fade" id="modalId" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
-                            <div class="modal-dialog modal-md modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header p-2 px-3">
-                                        <h5 class="modal-title" id="modalTitleId">Edit Details</h5>
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 
-                                    </div>
-                                    <div class="modal-body">
-                                        <form action="">
-                                            <div class="mb-3">
-                                                <label for="" class="form-label">Full Name</label>
-                                                <input type="text" name="" class="form-control" value="Bilal Obeid" id="">
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="" class="form-label">Username</label>
-                                                <input type="text" disabled class="form-control" value="@bilal" name="" id="">
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="" class="form-label">Email Addr.</label>
-                                                <input type="text" class="form-control" value="bilal@gmail.com" name="" id="">
-                                            </div>
-                                            <div class="mb-3">
-                                                <label for="" class="form-label">Your Photo</label>
-                                                <input type="file" name="" id="">
-                                            </div>
-                                            <div class="d-flex justify-content-end">
-                                                <input type="submit" class="btn btn-sm btn-danger" value="Save Edit">
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> -->
+<!-- Modal -->
+<div class="modal fade" id="modalId" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+    <?php include("modals/info-modal.php") ?>
+</div>
---
+++ modals/info-modal.php
@@ -0,0 +1,32 @@
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header p-2 px-3">
+                <h5 class="modal-title" id="modalTitleId">Edit Details</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    <div class="mb-3">
+                        <label for="" class="form-label">Full Name</label>
+                        <input type="text" name="" class="form-control" value="Bilal Obeid" id="">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Username</label>
+                        <input type="text" disabled class="form-control" value="@bilal" name="" id="">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Email Addr.</label>
+                        <input type="text" class="form-control" value="bilal@gmail.com" name="" id="">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">Your Photo</label>
+                        <input type="file" name="" id="">
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <input type="submit" class="btn btn-sm btn-danger" value="Save Edit">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
